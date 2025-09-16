d839ad84d7fa154f6722836d93c3e4e0175510fa
rupamsutar <rupamsutar281998@gmail.com>
2025-09-16 21:43:56 +0530

    Restaurant Card Complete

--- app.js
+++ app.js
@@ -41,14 +41,20 @@ const Header = () => {
     )
 }
 
-const styleCard = {
-    backgroundColor: "#f0f0f0",
-}
-
 const RestaurantCard = () => {
     return (
-        <div className="res-card" style={styleCard}>
+        <div className="res-card" style={{
+            backgroundColor: "#f0f0f0",
+        }}>
+            <img 
+                src="https://figarooliveoil.com/wp-content/uploads/2022/02/chicken-dum-biryani-2.png"
+                className="res-logo"
+                alt="res-logo"
+            />
             <h3>Meghana Foods</h3>
+            <h4>Biryani, North Indian, Mughlai</h4>
+            <h4>4.5 ⭐</h4>
+            <h4>38 mins</h4>
         </div>
     )
 }
