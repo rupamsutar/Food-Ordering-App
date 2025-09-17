b32335acecb4d37b4ecc2bf34d34237fb80cc05c
rupamsutar <rupamsutar281998@gmail.com>
2025-09-17 23:04:45 +0530

    Calling the live swiggy API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const Body = () => {
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+    const fetchRestaurantData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.636211&lng=73.7663474&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const jsonData = await data.json();
+        const filteredData = jsonData?.data?.cards.filter((card) => {
+            return card.card.card.id === "restaurant_grid_listing_v2";
+        });
+
+        const restaurantsData = filteredData[0].card.card.gridElements.infoWithStyle.restaurants;
+        setListOfRestaurants(restaurantsData);
+        
+        console.log(restaurantsData);
+    }
+
+    useEffect(() => {
+        fetchRestaurantData();
+    }, []);
 
     return (
         <div className='body'>
@@ -22,7 +38,7 @@ const Body = () => {
                 >Top Rated Restaurant</button>
                 <button 
                     className="filter-btn"
-                    onClick={() => setListOfRestaurants(resList)}                
+                    onClick={() => fetchRestaurantData()}                
                 >Show All Restaurants</button>
             </div>
             <div className="res-container">
