89b22ec75807434d406af6828a961b96ebbbe537
rupamsutar <rupamsutar281998@gmail.com>
2025-09-20 09:20:00 +0530

    updated parcel for react router dom

--- src/app.js
+++ src/app.js
@@ -2,6 +2,24 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const appRouter = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <AppLayout />
+        },
+        {
+            path: "/about",
+            element: <About />
+        },
+        {
+            path: '/contact',
+            element: <Contact />
+        }
+    ]
+)
  
 const AppLayout = () => {
     return (
@@ -13,4 +31,4 @@ const AppLayout = () => {
 }
 
 const root = createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = (props) => {
+    return (
+        <div>
+            <h1>Hey this is me, Rupam Sutar, on this side.</h1>
+            <h2>This is the About Us Page</h2>
+        </div>
+    )
+};
+
+export default About;
